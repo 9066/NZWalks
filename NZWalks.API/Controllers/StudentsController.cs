fcdf966c38c7eb86c5b9a8d36bf2223971d76e0a
Brajesh Kumar Mishra <brajesh.kumar@inubesolutions.com>
2025-02-17 09:10:40 +0530

    Project setup

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https:localhost:portNumber/api/Student
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudent()
+        {
+            string[] studentNames = new string[] { "Brajesh", "Suyash", "Dinkar" };
+            return Ok(studentNames); 
+        }
+    }
+}
---
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+using System.Data;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        //public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        //{
+        //    // Optional: Add this to get more detailed connection errors
+        //    this.Database.GetDbConnection().StateChange += (sender, e) =>
+        //    {
+        //        if (e.CurrentState == ConnectionState.Broken)
+        //        {
+        //            Console.WriteLine($"Database connection state changed to: {e.CurrentState}");
+        //        }
+        //    };
+        //}
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
